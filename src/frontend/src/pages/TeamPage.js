d341f259247bc7f6988db2a4046df3313018fc5c
Juanma <jmlejarriaga@gmail.com>
2021-07-23 10:51:42 +0200

    1. Batch created to populate in memory database from csv file 2. AfterJob class to populate Team database 3. Controller created 4. React app created 5. First approach to React front 6. Links added to access info of any team showed

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,28 +1,34 @@
 import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallDetailCard } from '../components/MatchSmallDetailCard';
 
 export const TeamPage = () => {
 
-    let [team, setTeam] = useState({matches: []});
+    const [team, setTeam] = useState({matches: []});
+    const { teamName } = useParams();
 
     useEffect(
         () => {
             const fetchMatches = async() => {
-                const response = await fetch('http://localhost:8080/team/Deccan Chargers');
+                const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
 
             };
             fetchMatches();
-        }, []
+
+        }, [teamName]
     );
 
-  return (
-    <div className="TeamPage">
-        <h1>{team.teamName}</h1>
-        <MatchDetailCard match={team.matches[0]}/>
-        {team.matches.slice(1).map(match => <MatchSmallDetailCard match={match} />)}
-    </div>
-  );
+    if(!team || !team.teamName) {
+        return <h1>Team not found</h1>
+    }
+    return (
+        <div className="TeamPage">
+            <h1>{team.teamName}</h1>
+            <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+            {team.matches.slice(1).map(match => <MatchSmallDetailCard teamName={team.teamName} match={match} />)}
+        </div>
+    );
 }
